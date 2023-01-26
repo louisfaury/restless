0ad853da3f5e8b1d37814192902c2706405c6d4c
l.faury <l.faury@criteo.com>
2023-01-26 23:51:53 +0100

    Implement basic agent structure

---
+++ restless/agent/agent.py
@@ -0,0 +1,40 @@
+"""
+Basic agent structure
+"""
+
+from abc import abstractmethod
+
+
+class Agent:
+    """
+    Basic agent class
+    """
+
+    def __init__(self, n_arms: int):
+        self.n_arms = n_arms
+
+    @abstractmethod
+    def act(self) -> int:
+        """
+        Play an arm
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, arm: int, state: int) -> None:
+        """
+        Incorporate signal of the arm that was pulled
+        """
+        raise NotImplementedError
+
+
+class StubbornAgent(Agent):
+    """
+    Agent that constantly plays the same action
+    """
+
+    def act(self) -> int:
+        return 0
+
+    def update(self, arm: int, state: int) -> None:
+        pass
