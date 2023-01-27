a59b8d7e53e941775ddbf717e27d6f00bcd819d0
l.faury <l.faury@criteo.com>
2023-01-27 11:51:49 +0100

    Packaging

--- restless/agents/__init__.py
+++ restless/agents/__init__.py
@@ -2,5 +2,6 @@
 Agent module
 """
 
+from restless.agents.agent import Agent
 from restless.agents.myopic_agent import Myopic
 from restless.agents.best_stationary_agent import BestStationaryArmAgent
