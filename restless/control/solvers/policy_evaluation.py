112940a5e80abc1d7d4f4bfeb2b921fdb9a8939e
l.faury <l.faury@criteo.com>
2023-02-28 21:35:55 +0100

    [Doc] Additional doc

--- restless/control/solvers/policy_evaluation.py
+++ restless/control/solvers/policy_evaluation.py
@@ -1,6 +1,5 @@
 """
 Policy evaluation for finite horizon, discounted and average MDPs
-TODO:
 """
 from typing import Tuple, Union
 
@@ -13,13 +12,17 @@ from restless.control.policy import Policy
 
 
 def get_reward_vector(pi: Policy, mdp: MDP) -> np.array:
-    # # Sets up the vector based representation of the policy's reward vector on some MDP
+    """
+    Sets up the vector based representation of the policy's reward vector on some MDP
+    """
     reward_vector = np.array([mdp.reward_function[pi.actions[state]][state] for state in range(mdp.n_states)])
     return reward_vector
 
 
 def get_transition_matrix(pi: Policy, mdp: MDP) -> np.array:
-    # Instantaneous Transition Matrix
+    """
+    Instantaneous Transition Matrix of a policy on some MDP
+    """
     transition_matrix = np.vstack(
         [mdp.transition_kernel[pi.actions[state]][state, :] for state in range(mdp.n_states)]
     )
@@ -27,6 +30,23 @@ def get_transition_matrix(pi: Policy, mdp: MDP) -> np.array:
 
 
 def discounted_policy_evaluation(pi: Policy, mdp: DiscountedMDP) -> np.array:
+    """
+
+    Parameters
+    ----------
+    pi : Policy
+        The policy to evaluate
+    mdp : MarkovDecisionProcess
+
+    Returns
+    -------
+    : np.array
+        The policy `pi`'s value
+
+    TODO
+    ----
+    Implement an iterative version (fixed-point solver) for large MDPs to avoid inverting enormous matrix.
+    """
     lambd = mdp.discount
     transition_matrix = get_transition_matrix(pi, mdp)
     reward_vector = get_reward_vector(pi, mdp)
@@ -39,12 +59,29 @@ def average_reward_policy_evaluation(
     """
     Policy evaluation for an average-gain MDP
 
-    .. warning:: Assumes that the policy's gain is constant,
-        i.e that the policy induces a Markov chain with a single recurrent class.
-        If unsure about this property, you can pass a parameter (:check_nb_irreducible_class:) that will trigger
-        an error if the chain's has several irreducible class.
+    Warnings
+    --------
+    Assumes that the policy's gain is constant,
+    i.e that the policy induces a Markov chain with a single recurrent class.
+    If unsure about this property, you can pass a parameter (`check_nb_irreducible_class`) that will trigger
+    an error if the chain's has several irreducible class.
 
-    Returns the policy's constant gain, as well as the bias vector if return_bias is set to True.
+    Parameters
+    ----------
+    pi : Policy
+        The policy to evaluate
+    mdp : MarkovDecisionProcess
+    return_bias : bool
+        Whether to return the policy's differential value function
+    check_nb_irreducible_class : bool
+        If True, checks that the policy's induces a Markov Chain with a unique irreductible class
+
+    Returns
+    -------
+    g : float
+         The policy's gain
+    h : np.array
+         The policy's differential value function if `return_bias` is set to True.
     """
     transition_matrix = get_transition_matrix(pi, mdp)
 
